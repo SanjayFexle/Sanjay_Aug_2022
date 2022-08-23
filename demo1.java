d34b344fc0dce333def47da5fdf3ed88e635d40b
SanjayFexle <111354692+SanjayFexle@users.noreply.github.com>
2022-08-23 11:32:18 +0530

    23Aug-1commit

---
+++ demo1.java
@@ -0,0 +1,12 @@
+import java.net.Socket;
+
+import javax.sound.midi.Soundbank;
+
+class Demo1{
+    public static void main(String[] args) {
+        System.out.println("Hello my first code.");
+        System.out.print("My name is palash");
+        System.out.println("i Am  working in Fexle.");
+        System.out.println("My name is palash2");
+    }
+}
