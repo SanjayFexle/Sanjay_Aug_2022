204d84468f81cc5e510593837eb210013a36c764
SanjayFexle <111354692+SanjayFexle@users.noreply.github.com>
2022-08-23 12:10:20 +0530

    Devloper2-FirstCommit

--- demo1.java
+++ demo1.java
@@ -1,9 +1,18 @@
-import java.net.Socket;
-
-import javax.sound.midi.Soundbank;
-
 class Demo1{
+    static String name;
+    static String Mname;
+    static String Lname;
+    static int Age;
+    static int phone;
     public static void main(String[] args) {
+        name="Sanjay";
+        Mname="Singh";
+        Lname="Rawat";
+        Age=25;
+        phone= 8949567;
+        System.out.println(name+" "+Mname+" "+Lname);
+        System.out.println(Age);
+        System.out.println(phone);
         System.out.println("Hello my first code.");
         System.out.print("My name is palash");
         System.out.println("i Am  working in Fexle.");
